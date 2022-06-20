edbda5a5bd9d5f80085e44e8aa65ce8b0f96439f
Tiedye <95548557+TiedyeCoding@users.noreply.github.com>
2022-06-20 19:58:34 -0400

    starter code

---
+++ index.js
@@ -0,0 +1,53 @@
+// heroku
+
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Server up and running!");
+// });
+
+// app.listen(process.env.PORT || 8000, () => {
+//   console.log("Listening on port 8000!");
+// });
+
+// bot code
+
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Intents } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS]
+});
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+client.login(token);
